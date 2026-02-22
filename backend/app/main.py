7defbd224da88aab7854da036efbda5c607a035c
Ganeshkumar-k-s <ganeshkumar78602005@gmail.com>
2026-02-22 12:37:41 +0530

    CORS middleware added

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-#from middlewares.auth import AuthMiddleware
+from middlewares.auth import AuthMiddleware
 from app.middlewares.logging import LoggingMiddleware
 from app.middlewares.rate_limit import RateLimitMiddleware
 
 from app.routes.auth_routes import auth_router
+from app.routes.form_routes import form_router
 
 app = FastAPI()
 
-#app.add_middleware(AuthMiddleware)
+app.add_middleware(AuthMiddleware)
 app.add_middleware(RateLimitMiddleware)
 app.add_middleware(LoggingMiddleware)
+app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:3000"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+
 
 app.include_router(auth_router)
+app.include_router(form_router)
